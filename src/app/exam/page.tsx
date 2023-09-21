1761c619740967cf7283aad8231ee3ecb6cc674a
unkabogaton <anthonralph2@gmail.com>
2023-09-21 17:45:07 +0800

    Create page.tsx

---
+++ src/app/exam/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import OutlinedCard from "../../components/OutlinedCard";
+
+const index = () => {
+  return (
+    <>
+      <OutlinedCard>Inter</OutlinedCard>
+    </>
+  );
+};
+
+export default index;
