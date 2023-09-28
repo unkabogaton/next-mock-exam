8e35de5d60e2d8a6821d45d9ce7dc2b5497c20db
unkabogaton <anthonralph2@gmail.com>
2023-09-29 02:13:04 +0800

    setup react-query

--- src/app/manage/layout.tsx
+++ src/app/manage/layout.tsx
@@ -3,6 +3,7 @@
 import { Box } from "@mui/material";
 import { ReactNode } from "react";
 import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   const queryClient = new QueryClient();
@@ -12,6 +13,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <Box sx={{ minHeight: "100vh" }} mb={15}>
         {children}
       </Box>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
