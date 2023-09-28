70ccb9b9cba4cba020a222c09bc7555a50348e2b
unkabogaton <anthonralph2@gmail.com>
2023-09-29 02:15:24 +0800

    format

--- src/app/exam/layout.tsx
+++ src/app/exam/layout.tsx
@@ -1,6 +1,5 @@
 import { Container, Box } from "@mui/material";
 import { ReactNode } from "react";
-
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <Container fixed>
