e10c4f834e139c2a3e0ee14d67bc9ee6bea33732
unkabogaton <anthonralph2@gmail.com>
2023-09-29 02:13:54 +0800

    moved firebase config file

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
@@ -14,4 +13,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const db = getFirestore(app);
+export default db;
