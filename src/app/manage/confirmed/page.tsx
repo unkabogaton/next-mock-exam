ed5d803b0b42e56e881364da8d60b024699dcae4
unkabogaton <anthonralph2@gmail.com>
2023-09-29 16:26:06 +0800

    Update page.tsx

--- src/app/manage/confirmed/page.tsx
+++ src/app/manage/confirmed/page.tsx
@@ -1,16 +1,38 @@
 "use client";
 
-import React from "react";
-import useQuestions from "@/hooks/useQuestions";
+import { useState } from "react";
+import usePaginatedQuestions from "@/hooks/usePaginatedQuestions";
 
 const ConfirmedQuestions = () => {
-  const { isLoading, data: questions } = useQuestions();
+  const [page, setPage] = useState(1);
+  const queryProps = {
+    order: "createdAt",
+    exam: "LLE",
+    userId: "54321",
+    approved: true,
+    page: page,
+    keywords: [],
+  };
+  const {
+    isLoading,
+    data: questions,
+    count,
+  } = usePaginatedQuestions(queryProps);
+
   if (isLoading) {
     return <h1>Loading...</h1>;
   }
   return (
     <>
       <h1>Confirmed Questions</h1>
+      <div>{count}</div>
+      {questions?.map((question) => (
+        <div key={question?.id}>
+          <h3>{question?.question}</h3>
+        </div>
+      ))}
+      <button onClick={() => setPage(page - 1)}>Prev</button>
+      <button onClick={() => setPage(page + 1)}>Next</button>
     </>
   );
 };
