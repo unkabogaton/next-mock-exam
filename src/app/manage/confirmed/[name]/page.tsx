778f9b7d9f97a6573981a2e5fc728297b6a742a4
unkabogaton <anthonralph2@gmail.com>
2023-09-29 16:25:54 +0800

    create page for paginated exam view

---
+++ src/app/manage/confirmed/[name]/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useState } from "react";
+import usePaginatedQuestions from "@/hooks/usePaginatedQuestions";
+
+const ExamQuestions = ({ params }: { params: { name: string } }) => {
+  const [page, setPage] = useState(1);
+  const queryProps = {
+    order: "createdAt",
+    exam: params.name,
+    userId: "54321",
+    approved: true,
+    page: page,
+    keywords: [],
+  };
+  const {
+    isLoading,
+    data: questions,
+    count,
+  } = usePaginatedQuestions(queryProps);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  return (
+    <>
+      <h1>Confirmed Questions</h1>
+      <div>{count}</div>
+      {questions?.map((question) => (
+        <div key={question?.id}>
+          <h3>{question?.question}</h3>
+        </div>
+      ))}
+      <button onClick={() => setPage(page - 1)}>Prev</button>
+      <button onClick={() => setPage(page + 1)}>Next</button>
+    </>
+  );
+};
+
+export default ExamQuestions;
