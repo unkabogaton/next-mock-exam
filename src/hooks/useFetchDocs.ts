7300936125722ac975435d569b8d577f2b58905a
unkabogaton <anthonralph2@gmail.com>
2023-09-29 16:25:22 +0800

    Create useFetchDocs.ts

---
+++ src/hooks/useFetchDocs.ts
@@ -0,0 +1,33 @@
+import { collection, orderBy, query } from "firebase/firestore";
+import iterateFetch from "./IterateFetch";
+import db from "@/firebase";
+import { useQuery } from "react-query";
+
+type fetchDocsProps = {
+  nestedCollection: [string, string, string];
+  singleCollection: string;
+  order: string;
+};
+
+const useFetchDocs = (props: fetchDocsProps) => {
+  const { nestedCollection, singleCollection, order } = props;
+  const fetchData = async () => {
+    let fetchCollection = collection(db, "exams");
+    if (nestedCollection) {
+      fetchCollection = collection(db, ...nestedCollection);
+    } else if (singleCollection) {
+      fetchCollection = collection(db, singleCollection);
+    }
+    const collectionQuery = query(fetchCollection, orderBy(order));
+    const { cleanedData } = await iterateFetch(collectionQuery);
+    return cleanedData;
+  };
+  return useQuery(["fetchDocs", singleCollection], fetchData, {
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    keepPreviousData: true,
+    refetchOnReconnect: false,
+  });
+};
+
+export default useFetchDocs;
