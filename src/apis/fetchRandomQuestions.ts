e9236f9a9e8053ba4c017026634b73796186cee8
unkabogaton <anthonralph2@gmail.com>
2023-09-30 23:20:18 +0800

    Update fetchRandomQuestions.ts

--- src/apis/fetchRandomQuestions.ts
+++ src/apis/fetchRandomQuestions.ts
@@ -1,6 +1,42 @@
+import db from "@/firebase";
+import { collection, limit, query, where } from "firebase/firestore";
+import iterateFetch from "@/apis/iterateFetch";
 
-const fetchRandomQuestions = () => {
-  return "";
+interface QuestionsProps {
+  id: string;
+  question: string;
+  created?: object;
+  approval?: object;
+  indexNumber: number;
+  category: string;
+  choices: choicesProps[];
+}
+
+interface choicesProps {
+  choice: string;
+  isSelected?: boolean;
+  point: number;
+}
+
+const fetchRandomQuestions = async (
+  examId: string,
+  category: string,
+  randomIdArray: number[]
+) => {
+  const questionsCollections = collection(db, "exams", examId, "questions");
+  const constraints = [
+    where("category", "==", category),
+    where("approved", "==", true),
+    where("indexNumber", "in", randomIdArray),
+    limit(10),
+  ];
+  const questionsQuery = query(questionsCollections, ...constraints);
+  const attributes = ["id", "question", "choices"];
+  const { cleanedData }: { cleanedData: QuestionsProps[] } = await iterateFetch(
+    questionsQuery,
+    attributes
+  );
+  return cleanedData;
 };
 
 export default fetchRandomQuestions;
