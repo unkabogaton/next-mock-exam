8163eef34f1f2779da24895cb54c65dc48013b20
unkabogaton <anthonralph2@gmail.com>
2023-10-01 18:01:56 +0800

    fix usefetchdocs issues

--- src/hooks/useFetchDocs.ts
+++ src/hooks/useFetchDocs.ts
@@ -4,30 +4,40 @@ import db from "@/firebase";
 import { useQuery } from "react-query";
 
 type fetchDocsProps = {
-  nestedCollection: [string, string, string];
-  singleCollection: string;
-  order: string;
+  nestedCollection?: [string, string, string];
+  singleCollection?: string;
+  attributes?: string[];
+  order: ["string", "asc" | "desc"];
 };
 
-const useFetchDocs = (props: fetchDocsProps) => {
-  const { nestedCollection, singleCollection, order } = props;
+const useFetchDocs = ({
+  nestedCollection,
+  singleCollection = "exams",
+  order,
+  attributes = [],
+}: fetchDocsProps) => {
   const fetchData = async () => {
-    let fetchCollection = collection(db, "exams");
-    if (nestedCollection) {
-      fetchCollection = collection(db, ...nestedCollection);
-    } else if (singleCollection) {
-      fetchCollection = collection(db, singleCollection);
-    }
-    const collectionQuery = query(fetchCollection, orderBy(order));
-    const { cleanedData } = await iterateFetch(collectionQuery);
+    const fetchCollection = nestedCollection
+      ? collection(db, ...nestedCollection)
+      : collection(db, singleCollection);
+    const collectionQuery = query(fetchCollection, orderBy(...order));
+    const { cleanedData } = await iterateFetch(collectionQuery, [
+      ...attributes,
+    ]);
     return cleanedData;
   };
-  return useQuery(["fetchDocs", singleCollection], fetchData, {
-    refetchOnMount: false,
-    refetchOnWindowFocus: false,
-    keepPreviousData: true,
-    refetchOnReconnect: false,
-  });
+  return useQuery(
+    ["fetchDocs", nestedCollection, singleCollection],
+    fetchData,
+    {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      keepPreviousData: true,
+      refetchOnReconnect: false,
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    }
+  );
 };
 
 export default useFetchDocs;
