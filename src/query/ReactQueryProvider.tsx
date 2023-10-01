070955aea808ce6dc6a9400445a5e211f3c2337a
unkabogaton <anthonralph2@gmail.com>
2023-10-01 18:01:41 +0800

    Update ReactQueryProvider.tsx

--- src/query/ReactQueryProvider.tsx
+++ src/query/ReactQueryProvider.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { QueryClientProvider, QueryClient } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 import { ReactNode } from "react";
 
 const queryClient = new QueryClient();
 
 export const ReactQueryProvider = ({ children }: { children: ReactNode }) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </>
   );
 };
